0d990a284cf19db1dde3f7a9c76e349665faff27
unknown <espejo.gabriel779@gmail.com>
2022-09-20 17:21:12 -0700

    added models and migrations

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_migrate import Migrate
 def create_app():
     app = Flask(__name__)
+
+    #database config
+    
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Highway774@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
@@ -15,4 +26,3 @@ def create_app():
     app.register_blueprint(fact.bp)
 
     return app
-
